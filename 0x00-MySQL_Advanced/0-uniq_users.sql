ac27a0111dbc45183d1b75890794ed3e2b7fd7e8
Menor25 <ajirimenor@gmail.com>
2023-01-12 18:45:13 +0100

    Error corrected

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -3,6 +3,7 @@
 --      id: integer, never null, auto increment and primary key
 --      email: string (255 characters), never null and unique
 --      name: string (255 characters)
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
