1c2543edfafa675f7a24be7f5bc49fa095487f3d
Menor25 <ajirimenor@gmail.com>
2023-01-12 18:54:25 +0100

    List all bands with 'Glam rock' as their main style, ranked by their longevity

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,5 @@
+-- List all bands with 'Glam rock' as their main style, ranked by their longevity.
+SELECT band_name, IFNULL(split, 2022) - IFNULL(formed, 0) AS lifespan 
+FROM metal_bands
+WHERE style like '%Glam rock%'
+ORDER BY lifespan DESC;
